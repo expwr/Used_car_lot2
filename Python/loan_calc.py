c913d88ffb4b8340250bb46ed3b2c63d964c1ae9
JonFactor <Jon.Factor2@gmail.com>
2022-10-27 13:07:29 -0400

    10/27

--- Python/loan_calc.py
+++ Python/loan_calc.py
@@ -1,36 +1,51 @@
-Credit = int(input("Credit Score: "))
-APR = 0
+#Def Var
+price = 0
+length = 0
+intrest = 0
+downPay = 0
+TradeInYN = True
+TradeIn = 0
+Tax = 0.07
+Fees = 0
+credit = 0
+DTI = 0
+income = 0
+carLux = False
+FabPro = False
+PaintPro = False
+VinEtch = False
+RustPro = False
+# Inputs
+price = input("What is the cars price?:")
+length = input("How long would you like the loan to last?:")
+downPay = input("How much would you like to put down?:")
+if TradeInYN == True:
+    TradeIn = input("What price did you buy the car for")
+    TradeInTime = input("How lomg have you had this car")
+    TradeInCond = input("What is the comdition of the car")
 
-if Credit < 300:
-    print("Sorry you cannot apply for a Car loan at this time, Thank You for your time")
-elif Credit < 500:
-    APR =+ 20.43
-    print("Your rates are", APR,"%" )
-    
-    
-elif Credit < 600:
-    APR =+ 16.85
-    print("Your rates are", APR,"%" )
-    
+# Fees
 
-elif Credit < 660:
-    APR =+ 10.33
-    print("Your rates are", APR,"%" )
-
-elif Credit < 780:
-    APR =+ 5.53
-    print("Your rates are", APR,"%" )
-    
-
-elif Credit < 850:
-    APR =+ 3.68
-    print("Your rates are", APR,"%" )
-    
-
-elif Credit > 850:
-    APR = .5
-    print("Your rates are", APR,"%" )
-
-
-
+# Title
+Fees += 15
+# Registration
+Fees += 10
+# Destination Charge
+Fees += 1250
+# document fee
+Fees += 250
+# ad fee
+if carLux == True:
+    Fees += .02 * price
+# Fabric protection
+if FabPro == True:
+    Fees += 300
+# Paint Protection
+if PaintPro == True:
+    Fees += 1250
+# vin etching
+if VinEtch == True:
+    Fees += 200
+if RustPro == True:
+    Fees += 400
 
