620810eaa0be08b504443b2b11338039650ef6d2
JonFactor <Jon.Factor2@gmail.com>
2022-11-07 13:22:09 -0500

    11/7 Start of loan est

--- sunrise-used-auto/src/Pages/LoanEst.js
+++ sunrise-used-auto/src/Pages/LoanEst.js
@@ -1,11 +1,18 @@
 import React from "react";
 
 function LoanEst() {
+  
   return (
     <div className="LoanEst">
-      <h1>Loans</h1>
+      <h1>Estimator</h1>
+      <form>
+        <div class='loanEstBox'></div>
+      </form>
     </div>
   );
 }
+
+
+
 
 export default LoanEst;
