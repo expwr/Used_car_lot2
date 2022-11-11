849a300d1525c836605b69748b7bf24d0bbe07ed
JonFactor <Jon.Factor2@gmail.com>
2022-11-11 13:17:55 -0500

    11/11

--- sunrise-used-auto/src/Pages/LoanEst.js
+++ sunrise-used-auto/src/Pages/LoanEst.js
@@ -7,23 +7,54 @@ function LoanEst() {
       
       <div class='loanEstBox'>
         <div class='TopBar'>
-            <title class='Title'>Estimator</title>
+            <title class='Title'>Loan Estimator</title>
+        </div>
+        <div className="inputs">
+          <div class='CarPrice'>
+            <p class='PriceText'>Car Price:</p>
+            <input type='text' class='PriceInput'></input>
           </div>
-          <div class='CreditBox'>
-            <input type='text'class='CreditInput'></input>
+          <div class='DownPay'>
+            <p class='DownPayText'>Down Payment:</p>
+            <input type='text' class='DownPayInput'></input>
+          </div>
+          <div class='LLength'>
+            <p class='LLengthText'>Lenght:</p>
+            <input type='text' class='LLengthInput'></input>
+          </div>
+          <div class='Credit'>
+            <p class='CreditText'>Credit Score:</p>
+            <input type='text' class='CreditInput'></input>
+          </div>
+        </div>
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          {/*  Old Design
+           <div class='CreditBox'>
+            <input type='text'class='CreditInput' placeholder='720'></input>
+            <button type='submit'></button>
             <h1 class='CredText'>Credit Score</h1>
           </div>
           <div class='priceBox'>
-            <input type='text'class='priceInput'></input>
+            <input type='text'class='priceInput'placeholder='10,000'></input>
             <h1 class='priceText'>Car Price</h1>
           </div>
           <div class='lengthBox'>
-            <input type='text'class='lengthInput'></input>
+            <input type='text'class='lengthInput'placeholder='60 months'></input>
             <h1 class='lengthText'>Loan Length</h1>
           </div>
           <div class='DownPayBox'>
-            <input type='text'class='DownPayInput'></input>
-            <h1 class='DownPayText'>Down Payment</h1>
+            <input type='text'class='DownPayInput' placeholder='Yes/No'></input>
+            <h1 class='DownPayText'>Down Payment?</h1>
           </div>
           <div class='TradeInBox'>
             <input type='text'class='TradeInInput'></input>
@@ -36,7 +67,7 @@ function LoanEst() {
           <div class='ExtraBox'>
             <input type='text'class='ExtraInput'></input>
             <h1 class='ExtraText'>Any Extras?</h1>
-          </div>
+          </div> */}
       </div>
     </div>
   );
