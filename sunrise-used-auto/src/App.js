189c181bf576b3dfe9d068453bbd0a3e425580f7
Chase Hutcheson <ChaseHutcheson2006@gmail.com>
2022-11-14 13:01:55 -0500

    Stuff anf auidbjiceburr

--- sunrise-used-auto/src/App.js
+++ sunrise-used-auto/src/App.js
@@ -1,24 +1,12 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom"
-import Home from './Pages/Home';
-import Cars from './Pages/Cars';
-import Reps from './Pages/Reps';
-import LoanEst from './Pages/LoanEst';
-import Account from './Pages/Account';
-import Support from './Pages/Support';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import NavBarComp from './Components/NavBar';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path="/Cars" element={<Cars />}/>
-        <Route path="/Representatives" element={<Reps />}/>
-        <Route path="/Loan-Estimator" element={<LoanEst />}/>
-        <Route path="/Account" element={<Account />}/>
-        <Route path="/Support" element={<Support />}/>
-      </Routes>
-    </Router>
+    <div className='App'>
+      <NavBarComp />
+    </div>
   )
 }
 
--- sunrise-used-auto/src/Components/NavBar.js
+++ sunrise-used-auto/src/Components/NavBar.js
@@ -4,12 +4,25 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import logo from './Sunrise Long.png'
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link
+} from "react-router-dom";
+import Home from "../Pages/Home";
+import Cars from "../Pages/Cars";
+import Reps from "../Pages/Reps";
+import LoanEst from "../Pages/LoanEst";
+import Account from "../Pages/Account";
+import Support from "../Pages/Support";
 
 export default class NavBarComp extends Component {
   render() {
     return (
+      <Router>
       <div>
-        <Navbar bg="light" expand="md">
+        <Navbar bg="info" expand="md">
             <Container>
             <Navbar.Brand href="/">
                 <img 
@@ -23,8 +36,12 @@ export default class NavBarComp extends Component {
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-10">
-                <Nav.Link href="/" color='white'>Home</Nav.Link>
-                <Nav.Link href="/Cars">Cars</Nav.Link>
+                <Nav.Link as={Link} to={"/"} color='white'>Home</Nav.Link>
+                <Nav.Link as={Link} to={"/Cars"} color='white'>Cars</Nav.Link>
+                <Nav.Link as={Link} to={"/Reps"} color='white'>Representatives</Nav.Link>
+                <Nav.Link as={Link} to={"/LoanEst"} color='white'>LoanEst</Nav.Link>
+                <Nav.Link as={Link} to={"/Account"} color='white'>Account</Nav.Link>
+                <Nav.Link as={Link} to={"/Support"} color='white'>Support</Nav.Link>
                 <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">
@@ -41,6 +58,15 @@ export default class NavBarComp extends Component {
         </Container>
     </Navbar>
       </div>
+      <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/Cars' element={<Cars/>}/>
+          <Route path='/Reps' element={<Reps/>}/>
+          <Route path='/LoanEst' element={<LoanEst/>}/>
+          <Route path='/Account' element={<Account/>}/>
+          <Route path='/Support' element={<Support/>}/>
+        </Routes>
+      </Router>
     )
   }
 }
